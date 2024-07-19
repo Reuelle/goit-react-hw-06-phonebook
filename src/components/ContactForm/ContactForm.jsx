4e1cd4f690cffee5a1742752f509c756a96aad88
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-19 11:45:42 +0800

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -14,34 +14,33 @@ export const ContactForm = () => {
     const name = form.elements.name.value;
     const number = form.elements.number.value;
     form.reset();
-
-    if (contacts.some(contact => contact.name === name)) {
+    if (contacts.value.find(contact => contact.name === name)) {
       alert(`${name} is already in contacts`);
-      return;
+      return false;
     }
-
-    dispatch(addContact({ name, number }));
+    dispatch(addContact(name, number));
+    return true;
   };
 
   return (
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.formLabel}>Name</label>
+      <label className={css.formLabel}>Name </label>
       <input
         className={css.formName}
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash, and spaces. For example: Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
         placeholder="Enter name"
       />
-      <label className={css.formLabel}>Number</label>
+      <label className={css.formLabel}>Number </label>
       <input
         className={css.formNumber}
         type="tel"
         name="number"
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses, and can start with +"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
         placeholder="Enter phone number"
       />
@@ -50,8 +49,4 @@ export const ContactForm = () => {
       </button>
     </form>
   );
-};
-
-ContactForm.propTypes = {
-  contacts: PropTypes.array.isRequired,
 };
