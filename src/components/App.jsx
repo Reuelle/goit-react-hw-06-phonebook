b8c6d0025f92ac35f5fbf7e207b554d93c2e24e0
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-07-19 10:37:11 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import './App.module.css';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
 export const App = () => {
   return (
-    <div className="app">
-      <div className="wrapper">
-        <h1 className="title">Phonebook</h1>
-        <ContactForm />
-        <h2 className="subtitle">Contacts</h2>
-        <Filter />
-        <ContactList />
-      </div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2> Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
